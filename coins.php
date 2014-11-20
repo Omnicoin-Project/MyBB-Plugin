d2d2a0dec874ac2172fdd2af1bce610c07d8b1f4
MeshCollider <dobsonsa68@gmail.com>
2014-11-20 20:35:21 +1300

    Update coins.php

--- coins.php
+++ coins.php
@@ -31,6 +31,10 @@ if (!$mybb->user['uid']) {
 	error_no_permission();
 }
 
+if (isset($mybb->input['omc_address']) && isset($mybb->input['omc_signature'])) {
+omnicoin_add_address($mybb->input['omc_address'], $mybb->input['omc_signature']);
+}
+
 if (isset($mybb->input['action'])) {
 	if ($mybb->input['action'] == "history") {
 		if (isset($mybb->input['uid'])) {
